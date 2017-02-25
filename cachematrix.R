6921cce00d9c17532fb1ffe88a2f63d8cfac91ea
DerrickAttfield <DerrickAttfield@yahoo.com>
2017-02-25 23:47:55 +0200

    Update cachematrix.R
    The completed Week 3 assignment

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,41 @@
 ## functions do
 
 ## Write a short comment describing this function
+#this function manAges the caches
 
 makeCacheMatrix <- function(x = matrix()) {
-
+##
+      inv <- NULL
+      #message(inv)
+      set <- function(im) {
+        x <<- im
+        message('in set function')
+        inv <<- NULL
+      }
+      get <- function() x
+      setinv <- function(solve) inv <<- solve
+      getinv <- function() inv
+      list(set = set, get = get,
+           setinv = setinv,
+           getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#cacheSolve checks to see if the argument matrix is identical to the previous
+#argument supplied  - if it is not identical it calculates the inverse of the matrix
+#                   - if it is identical it retrieves the cached inverse
+cacheSolve <- function(x, ...,newZ) {
+    data <- x$get()
+      if(identical(data,newZ)==TRUE){
+        message("getting cached data")
+        inv <- x$getinv()
+        return(inv)
+      } else if(identical(data,newZ)==FALSE){
+        message("recalculating matrix inverse")
+        inv <- solve(newZ, ...)
+        x$set(newZ)
+        x$setinv(inv)
+        return(inv)
+      }
 }
